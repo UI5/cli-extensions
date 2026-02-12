2c00ef2f7ae66b6d37ca19087cddb051f887e7c1
Gianluca Beil <g.beil@sap.com>
2026-02-12 14:24:46 +0100

    ci: Add comments to knip.config.js

--- knip.config.js
+++ knip.config.js
@@ -1,6 +1,7 @@
 const config = {
 	/**
-	 * As we currently only need unused dependency checks, we disable all checks except for that
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
 	 */
 	rules: {
 		files: "off",
@@ -16,7 +17,16 @@ const config = {
 	},
 
 	ignoreDependencies: [
+		/**
+		 * Used in packages/middleware-code-coverage/test/integration/fixtures/ui5-app/package.json
+		 * which is not part of the scope that knip analyzes
+		 */
 		"@ui5/cli",
+
+		/**
+		 * Used via nyc ava --node-arguments="--experimental-loader=@istanbuljs/esm-loader-hook"
+		 * which is not detected by knip as a usage of this package
+		 */
 		"@istanbuljs/esm-loader-hook"
 	],
 
