34dcf164f0f4415941cf3830a165c6b64134b624
Gianluca Beil <66752411+werwolf2303@users.noreply.github.com>
2026-02-12 15:38:48 +0100

    ci: Replace depcheck with knip (#540)
    Due to the deprecation of depcheck we migrated to knip, which is in
    active development and has more features. As a first step we only
    activate the checking of unused dependencies.
    
    For monorepos it has to be run at root package level: [Knip
    Monorepos](https://knip.dev/features/monorepos-and-workspaces).
    
    JIRA: CPOUI5FOUNDATION-1126
    
    ---------
    
    Co-authored-by: Matthias Osswald <mat.osswald@sap.com>

---
+++ knip.config.js
@@ -0,0 +1,49 @@
+const config = {
+	/**
+	 * We only need dependency checking at the moment,
+	 * so all checks except for dependencies are turned off.
+	 */
+	rules: {
+		files: "off",
+		duplicates: "off",
+		classMembers: "off",
+		unlisted: "off",
+		binaries: "off",
+		unresolved: "off",
+		catalog: "off",
+		exports: "off",
+		types: "off",
+		enumMembers: "off",
+	},
+
+	ignoreDependencies: [
+		/**
+		 * Used in packages/middleware-code-coverage/test/integration/fixtures/ui5-app/package.json
+		 * which is not part of the scope that knip analyzes
+		 */
+		"@ui5/cli",
+
+		/**
+		 * Used via nyc ava --node-arguments="--experimental-loader=@istanbuljs/esm-loader-hook"
+		 * which is not detected by knip as a usage of this package
+		 */
+		"@istanbuljs/esm-loader-hook"
+	],
+
+	workspaces: {
+		"packages/middleware-code-coverage": {
+			/**
+			 * UI5 custom middleware entry (see packages/middleware-code-coverage/ui5.yaml)
+			 */
+			entry: ["lib/middleware.js"],
+			ava: {
+				config: [
+					"ava.config.js",
+					"ava-integration.config.js"
+				]
+			}
+		}
+	}
+};
+
+export default config;
